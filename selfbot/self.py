e89823a1f0ac55a740b58033916fd025ccf122b1
rm21845 <11241577+rm21845@users.noreply.github.com>
2019-02-03 11:06:42 -0500

    Update self.py

--- selfbot/self.py
+++ selfbot/self.py
@@ -4,6 +4,7 @@ import asyncio
 import uvloop
 import conf
 
+
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 client = discord.Client()
 
@@ -11,12 +12,6 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(client.user.name)
-    serverss = client.servers
-    for server in serverss:
-        members = server.members
-        if server.id == '452565448534392849':
-            for member in members:
-                print(member.name)
 
 
 async def del_msg(chan):
@@ -33,10 +28,6 @@ async def del_msg(chan):
 
 @client.event
 async def on_message(msg):
-    """?del deletes messages in given channel
-       ?prvdel will delete all the messages in all
-       of your private channels
-    """
     if msg.author != client.user:
         return 
     
